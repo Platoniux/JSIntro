7ac5ac8cafc3cf1f923039aa75303042d83efc3d
Platoniux <chizzza@gmail.com>
2018-12-13 11:24:46 +0200

    corrected style of writing code

--- script.js
+++ script.js
@@ -1,92 +1,81 @@
-// /*Task 2*/
-// const nameOfSchool = 'Mate Academy';
-// const namesOfCourses = ['MateAcademyFE', 'MateAcademyQA', 'MateAcademyJavaDev', 'MateAcademyPython'];
-// const nameOfGroup = 'fe_oct18';
-// const ourStudentsName = ['Roma', 'Veronika', 'Vania', 'Vlad', 'Alina', 'Cookies', 'Dimon', 'Julia', 'Sergey', 'Anton'];
+/*Task 2*/
+var nameOfSchool = 'Mate Academy';
+var namesOfCourses = ['MateAcademyFE', 'MateAcademyQA', 'MateAcademyJavaDev', 'MateAcademyPython'];
+var nameOfOurGroup = 'fe_oct18';
+var ourStudentsNames = ['Roma', 'Veronika', 'Vania', 'Vlad', 'Alina', 'Cookies', 'Dimon', 'Julia', 'Sergey', 'Anton'];
 
 
 
-// /*Task 3 In McDonalds*/
-// const sum = prompt('Скільки грошей ви маєте?', '');
-// const offer;
+/*Task 3 In McDonalds*/
+var sum = prompt('Скільки грошей ви маєте?', '');
+var offer;
 
-// if (sum >= 100) {
-//   offer = confirm('Ви можете купити БігМакМеню. Бажаєте?');
-  
-//   if(offer) {
-//     alert('Ви замовили БігМакМеню');
-//   } else {
-//     alert('Ви нічого не замовили');
-//   }
-
-// } else if (50 <= sum && sum < 100) {
-//   offer = confirm( 'Ви можете купити Чізбургер і картоплю. Бажаєте?');
-
-//   if(offer) {
-//     alert('Ви замовили Чізбургер і картоплю');
-//   } else {
-//     alert('Ви нічого не замовили');
-//   }
-
-// } else if (20 <= sum && sum < 50 ) {
-//   offer = confirm('Ви можете купити колу. Бажаєте?');
-
-//   if(offer) {
-//     alert('Ви замовили колу');
-//   } else {
-//     alert('Ви нічого не замовили');
-//   }
-
-// } else {
-//   alert('Пробачте, в нашому закладі немає страв для вас :(');
-// }
+if (sum >= 100) {
+  offer = confirm('Ви можете купити БігМакМеню. Бажаєте?');
+  if (offer) {
+    alert('Ви замовили БігМакМеню');
+  } else {
+    alert('Ви нічого не замовили');
+  }
+} else if (50 <= sum) {
+  offer = confirm( 'Ви можете купити Чізбургер і картоплю. Бажаєте?');
+  if (offer) {
+    alert('Ви замовили Чізбургер і картоплю');
+  } else {
+    alert('Ви нічого не замовили');
+  }
+} else if (20 <= sum) {
+  offer = confirm('Ви можете купити колу. Бажаєте?');
+  if (offer) {
+    alert('Ви замовили колу');
+  } else {
+    alert('Ви нічого не замовили');
+  }
+} else {
+  alert('Пробачте, в нашому закладі немає страв для вас :(');
+}
 
 
 
-// /*Task 5 same 'in McDonalds' task with console.log! BEFORE RUN IT, PLEASE COMMENT CODE ABOVE!*/
+/*Task 5 same 'in McDonalds' task with console.log! BEFORE RUN IT, PLEASE COMMENT CODE ABOVE!*/
 
-// if (sum >= 100) {
-//   offer = confirm('Ви можете купити БігМакМеню. Бажаєте?');
-  
-//   if(offer) {
-//     console.log('Ви замовили БігМакМеню');
-//   } else {
-//     console.log('Ви нічого не замовили');
-//   }
+if (sum >= 100) {
+  offer = confirm('Ви можете купити БігМакМеню. Бажаєте?');
+  if (offer) {
+    console.log('Ви замовили БігМакМеню');
+  } else {
+    console.log('Ви нічого не замовили');
+  }
+} else if (50 <= sum && sum < 100) {
+  offer = confirm( 'Ви можете купити Чізбургер і картоплю. Бажаєте?');
+  if (offer) {
+    console.log('Ви замовили Чізбургер і картоплю');
+  } else {
+    console.log('Ви нічого не замовили');
+  }
+} else if (20 <= sum && sum < 50 ) {
+  offer = confirm('Ви можете купити колу. Бажаєте?');
 
-// } else if (50 <= sum && sum < 100) {
-//   offer = confirm( 'Ви можете купити Чізбургер і картоплю. Бажаєте?');
+  if (offer) {
+    console.log('Ви замовили колу');
+  } else {
+    console.log('Ви нічого не замовили');
+  }
+} else {
+  console.log('Пробачте, в нашому закладі немає страв для вас :(');
+}
 
-//   if(offer) {
-//     console.log('Ви замовили Чізбургер і картоплю');
-//   } else {
-//     console.log('Ви нічого не замовили');
-//   }
-
-// } else if (20 <= sum && sum < 50 ) {
-//   offer = confirm('Ви можете купити колу. Бажаєте?');
-
-//   if(offer) {
-//     console.log('Ви замовили колу');
-//   } else {
-//     console.log('Ви нічого не замовили');
-//   }
-
-// } else {
-//   console.log('Пробачте, в нашому закладі немає страв для вас :(');
-// }
-
-// /*Task 6 */
-// const Masha, Stepan, Andry;
-// Stepan < Masha < Andry;
+/*Task 6 */
+var Masha, Stepan, Andry;
+Stepan < Masha < Andry;
 
 
 // /*Task 7 Sales*/
 
-const enterHour = function enterHourForTask7(question){
-  const a = +prompt(question, '')
-  if(isNaN(a) || a < 0 || a > 23) {
-    alert('Incorrect value')
+var enterHour = function enterHourForTask7(question){
+  var a = +prompt(question, '');
+  if (isNaN(a) || a < 0 || a > 23) {
+    alert('Incorrect value');
     enterHourForTask7(question, '');
   }
   return a;
@@ -94,11 +83,11 @@ const enterHour = function enterHourForTask7(question){
 
 
 
-const presentTime = enterHour('What time is it now?');
-const startDiscountTime = enterHour('What time did dicounts start?');
-const endDiscountTime = enterHour('What time will dicounts end?')
+var presentTime = enterHour('What time is it now?');
+var startDiscountTime = enterHour('What time did dicounts start?');
+var endDiscountTime = enterHour('What time will dicounts end?');
 
-if(startDiscountTime < endDiscountTime && presentTime >= startDiscountTime  && presentTime <=endDiscountTime) {
+if (startDiscountTime < endDiscountTime && presentTime >= startDiscountTime && presentTime <=endDiscountTime) {
   console.log('It\'s time for sales!!!');
 } else if (startDiscountTime > endDiscountTime && (presentTime >= startDiscountTime || presentTime <= endDiscountTime)) {
   console.log('It\'s time for sales!!!');
@@ -110,13 +99,13 @@ if(startDiscountTime < endDiscountTime && presentTime >= startDiscountTime  && p
 
 /*Task 8 Сapricious Masha*/
 
-//  const existOfAngel = confirm('Чи прийшла Анжела?');
-//  const existOfStepan = confirm('Чи прийшов Степан?');
-//  const existOfMiko = confirm('Чи пройшов Михайло?');
+ var existOfAngel = confirm('Чи прийшла Анжела?');
+ var existOfStepan = confirm('Чи прийшов Степан?');
+ var existOfMiko = confirm('Чи пройшов Михайло?');
 
-// if((existOfMiko && existOfAngel && existOfStepan) || (!existOfAngel && (!existOfMiko || !existOfStepan))) {
-//   alert('Маша засмучена!');
-// } else {
-//   alert('Маша щаслива!');
-// }
+if ((existOfMiko && existOfAngel && existOfStepan) || (!existOfAngel && (!existOfMiko || !existOfStepan))) {
+  console.log('Маша засмучена!');
+} else {
+  console.log('Маша щаслива!');
+}
 
